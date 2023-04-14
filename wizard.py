c45ac677344f0a2b670e032758a72e4d95e4382d
Nils Liebreich <nilsl99@mi.fu-berlin.de>
2023-04-14 15:32:46 +0200

    Added get_scores

--- wizard.py
+++ wizard.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import requests
+import http
 
 from authorization import get_pem_port, ConnectionError
 
@@ -44,11 +45,9 @@ class GameState:
             raise ConnectionError()
 
         if response.status_code == 200:
-            data = response.json()
+            return response.json()
         else:
             raise ConnectionError()
-
-        return data
 
     def get_team(self, player=None):
         if player is None:
@@ -80,6 +79,64 @@ class GameState:
                     if p["summonerName"] == name:
                         return p["team"]
 
+    def get_scores(self):
+        team = self.get_team()
+        scores = defaultdict(int)
+        ally_member = []
+        enemy_member = []
+
+        # Fetch basic game data
+        game_data = self.get_data("gamestats")
+        scores["gameMode"] = game_data["gameMode"]
+        scores["gameTime"] = game_data["gameTime"]
+
+        # Fetch player score
+        player_data = self.get_data("playerlist")
+
+        for player in player_data:
+            if player["team"] == team:
+                team_id = "ally"
+                ally_member.append(player["summonerName"])
+            else:
+                team_id = "enemy"
+                enemy_member.append(player["summonerName"])
+
+            for id, value in player["scores"].items():
+                scores[f"{team_id}_{player['position']}_{id}"] += value
+
+        # Fetch team scores
+        event_data = self.get_data("eventdata")["Events"]
+
+        for event in event_data:
+            if event["EventName"] in ["HeraldKill", "DragonKill", "BaronKill"]:
+                if event["KillerName"] in ally_member:
+                    scores[f"ally_{event['EventName']}"] += 1
+                else:
+                    scores[f"enemy_{event['EventName']}"] += 1
+
+            elif event["EventName"] == "Ace":
+                if event["AcingTeam"] == team:
+                    scores["ally_aces"] += 1
+                else:
+                    scores["enemy_aces"] += 1
+
+            elif event["EventName"] == "TurretKilled":
+                structure = event["TurretKilled"].split("_")[1]
+
+                if structure == "T2" and team == "ORDER" or structure == "T1" and team == "CHAOS":
+                    scores["ally_turrets"] += 1
+                else:
+                    scores["enemy_turrets"] += 1
+
+            elif event["EventName"] == "InhibKilled":
+                structure = event["InhibKilled"].split("_")[1]
+
+                if structure == "T2" and team == "ORDER" or structure == "T1" and team == "CHAOS":
+                    scores["ally_inhibs"] += 1
+                else:
+                    scores["enemy_inhibs"] += 1
+
+        return scores
 
 if __name__ ==  "__main__":
     try:
