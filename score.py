92f872798f46f3329faaef1736ba06f5b2544ef6
Nils Liebreich <nilsl99@mi.fu-berlin.de>
2023-04-14 23:06:50 +0200

    Added a script to save gamedata

---
+++ score.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import time
+
+from wizard import GameState
+from authorization import ConnectionError
+
+def main():
+    wizard = None
+    state = None
+    df = None
+    while True:
+        if wizard is None:
+            try:
+                wizard = GameState()
+            except ConnectionError:
+                time.sleep(1)
+                continue
+        else:
+            try:
+                state = wizard.check_game_state()
+            except ConnectionError:
+                wizard = None
+                time.sleep(1)
+                continue
+
+            if state == "InProgress":
+                if df is None:
+                    df = pd.DataFrame()
+
+                try:
+                    scores = wizard.get_scores()
+                except ConnectionError:
+                    wizard = None
+                    time.sleep(1)
+                    continue
+
+                new_row = pd.DataFrame([scores])
+
+                df = pd.concat([df, new_row], ignore_index=True)
+
+                time.sleep(10)
+
+            else:
+                if df is not None:
+                    mode = df["gameMode"][0]
+                    df.to_csv(f"GameData\\{mode}\\{time.strftime(r'%Y-%m-%d_%H_%M_%S')}.csv")
+                    df = None
