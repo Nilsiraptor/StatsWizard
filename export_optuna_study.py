07a68c8c2a4ecb62405d4139c43ca8f8be8953e9
Nils Liebreich <nilsl99@mi.fu-berlin.de>
2026-08-12 15:04:50 +0200

    Added small helper scripts

---
+++ export_optuna_study.py
@@ -0,0 +1,16 @@
+import optuna
+
+for mode in ["ARAM", "CLASSIC", "KIWI", "STRAWBERRY"]:
+    if mode != "ARAM": pass
+
+    try:
+        study = optuna.load_study(
+            study_name=mode,
+            storage="sqlite:///hyperparameter_search.db"
+        )
+
+        df = study.trials_dataframe()
+
+        df.to_csv(f"hyperparameter_search_{mode}.csv", index=False)
+    except:
+        pass
