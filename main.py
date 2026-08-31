c33f64a7c1e42575dd30a3ae06f4028736961105
Nils Liebreich <nilsl99@mi.fu-berlin.de>
2026-08-31 15:52:16 +0200

    Added darkmode

--- main.py
+++ main.py
@@ -3,18 +3,17 @@
 This module initializes the PyQt6 application, applies global styling,
 and creates the main window with its graph and statistics display.
 """
-import sys
-import subprocess
+
 import atexit
+import subprocess
+import sys
 
 from PyQt6.QtWidgets import QApplication
 
 from gui import MainWindow
 
-
 score_process = subprocess.Popen(
-    [sys.executable, "score.py"],
-    creationflags=subprocess.CREATE_NO_WINDOW
+    [sys.executable, "score.py"], creationflags=subprocess.CREATE_NO_WINDOW
 )
 
 atexit.register(score_process.terminate)
@@ -25,6 +24,7 @@ app = QApplication(sys.argv)
 app.setStyleSheet("""
     QWidget {
         background-color: white;
+        color: black;
         padding: 0px;
         margin: 0px;
     }
